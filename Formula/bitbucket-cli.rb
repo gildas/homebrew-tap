68ede12c52de0ce15a5e9ee1f9f68d9c96b3496d
Gildas Cherruel <gildas@breizh.org>
2024-12-29 22:19:05 +0900

    Formula: bitbucket-cli, version 0.12.0

--- Formula/bitbucket-cli.rb
+++ Formula/bitbucket-cli.rb
@@ -1,9 +1,9 @@
 class BitbucketCli < Formula
   desc "Bitbucket command-line tool"
   homepage "https://bitbucket.org/gildas_cherruel/bb"
-  url "https://bitbucket.org/gildas_cherruel/bb/get/9f4581f5c59c7da64a45e3385666e3df40fa8477.tar.gz"
-  version "0.11.0"
-  sha256 "2050ddbc6e112048a9aada2be34a544401005c033488a48983d74b3c54269750"
+  url "https://bitbucket.org/gildas_cherruel/bb/get/47a043f5a4ddb9cb9dcf13936d8c7a73644d2a70.tar.gz"
+  version "0.12.0"
+  sha256 "17e817b6cc2b65016c61270512acbca89ded53f710eb806196f100e6607a5243"
   license "MIT"
 
   depends_on "go" => :build
